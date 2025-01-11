b696888591362e140f83381759d6d21c458db0b9
Tomer Horowitz <tomerh@realcommerce.co.il>
2025-01-12 00:25:41 +0200

    refactor: rename variables for clarity in health check processing

--- src/index.ts
+++ src/index.ts
@@ -18,29 +18,29 @@ if (statusResult.data.status !== 'good') {
 	throw new Error('Tdarr\'s status is not good, cannot continue.');
 }
 
-const failedFiles = await getFailedHealthChecks();
-if (failedFiles.totalCount === 0) {
+const healthChecks = await getFailedHealthChecks();
+if (healthChecks.totalCount === 0) {
 	logger.info('No failed health checks, nothing to do.');
 	process.exit(0);
 }
 
-logger.child({count: failedFiles.totalCount}).info('Got failed health checks from Tdarr, going over them...');
-for (const failedFile of failedFiles.array) {
-	if (!failedFile.meta) {
-		logger.child({file: failedFile.file}).warn('Failed file does not have metadata, skipping...');
+logger.child({count: healthChecks.totalCount}).info('Got failed health checks from Tdarr, going over them...');
+for (const healthCheck of healthChecks.array) {
+	if (!healthCheck.meta) {
+		logger.child({file: healthCheck.file}).warn('No meta data, skipping...');
 		continue;
 	}
 
-	const {FileName} = failedFile.meta;
-	const movie = await getRadarrMovie(failedFile);
-	const series = await getSonarrSeries(failedFile);
+	const {FileName} = healthCheck.meta;
+	const movie = await getRadarrMovie(healthCheck);
+	const series = await getSonarrSeries(healthCheck);
 
 	if (movie?.id) {
-		logger.child({FileName, radarrId: movie.id}).info('Found file in Radarr');
+		logger.child({FileName, radarrId: movie.id}).info('File found in Radarr');
 	} else if (series?.id) {
-		logger.child({FileName, sonarrId: series.id}).info('Found file in Sonarr');
+		logger.child({FileName, sonarrId: series.id}).info('File found in Sonarr');
 	} else {
-		logger.child({FileName}).warn('Movie not found in Radarr or Sonarr, skipping...');
+		logger.child({FileName}).warn('File not found in Radarr or Sonarr, skipping...');
 	}
 }
 
